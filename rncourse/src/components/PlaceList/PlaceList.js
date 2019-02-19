bf2a9d6b6a4dcd3908570ca8b1253a9997cd579b
Daniel <Danielguzik6@gmail.com>
2019-02-19 19:25:48 +0100

    W31 - ScrollView, Add and Remove elements

---
+++ rncourse/src/components/PlaceList/PlaceList.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, View, ScrollView } from 'react-native';
+import ListItem from '../ListItem/ListItem';
+
+
+const placeList = props => {
+    const placeOutput = props.places.map((place, i) => (
+        <ListItem 
+            key={i} 
+            placeName={place} 
+            onItemPressed={() => props.onItemDeleted(i)} 
+        />
+    ));
+
+    return (
+        <ScrollView style={styles.listContainer}>{placeOutput}</ScrollView>
+    );
+};
+
+const styles = StyleSheet.create({
+    listContainer: {
+        width: "100%",
+    }
+});
+
+export default placeList;
